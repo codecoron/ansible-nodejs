590beaf4e48034a6ca9c09b5c62532a7489a3c68
Coderon_Lee <1710034886@qq.com>
2022-04-19 23:40:09 +0800

    [add]重启

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const express = require('express');
 // 创建应用
 const app = express();
 
+// # 解决跨域问题
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+
 // 设置路由
-app.get('/', (req, resp) => {
+app.post('/vm/restart', (req, resp) => {
     // 输出响应
     // resp.json(req.headers);
+    //执行重启
+    var spawn = require('child_process').spawn;
+    free = spawn('reboot');
+
     const hostinfo = require('./hostInfo.json')
     console.log(hostinfo)
     resp.json(hostinfo)
